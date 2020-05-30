73b6e3ea157cc011f7a7389043180b7533aaf4d4
laviniucoita <38728756+laviniucoita@users.noreply.github.com>
2020-05-30 21:37:25 +0300

    Add files via upload

--- Embedded/senzori.ino
+++ Embedded/senzori.ino
@@ -4,6 +4,8 @@
 #include "Adafruit_Si7021.h"
 #include <ESP8266WiFi.h>
 #include "FirebaseESP8266.h"
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 
 #define bound 115200
@@ -20,6 +22,7 @@ float senzor_gaz;
 float pulse_signal_bpm;
 float light_value;
 
+
 Adafruit_Si7021 sensorsi = Adafruit_Si7021();
 float temperatura;
 float umiditate;
@@ -28,13 +31,35 @@ PulseSensorPlayground pulseSensor;
 
 #define FIREBASE_HOST "medicalapp-36d7a.firebaseio.com" 
 #define FIREBASE_AUTH "LsX4OkwTR6Mn9uZunlJFzf0lOPNZEPyaFgr7Opsa" 
-#define WIFI_SSID "iPhone - Laviniu" 
-#define WIFI_PASSWORD "gicutza77" 
+#define WIFI_SSID "Laviniu" 
+#define WIFI_PASSWORD "keyboard" 
 
 FirebaseData firebaseData;
 
-const String cnp = "17788776674";
+const String cnp = "27788776674";
 const String path = "/Date_Medicale_Senzor";
+
+const int gas_sensor_limit = 70;
+const int pulse_higher_limit = 100;
+const int pulse_lower_limit = 70;
+
+String Hours;
+String Years;
+String Gas_Alert;
+String Pulse_Alert;
+
+//GMT+3
+const long utcOffsetInSeconds = 10800;
+//Week Days
+String weekDays[ 7 ] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+//Month names
+String months[ 12 ] ={"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+
 void setup()
 {
   Serial.begin( bound );
@@ -57,6 +82,9 @@ void setup()
     delay(500);
     Serial.print(".");
   }
+
+  timeClient.begin();
+  
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -67,9 +95,10 @@ void setup()
 
 }
 
+
 void loop()
 {
-
+ 
   temperatura = sensorsi.readTemperature();
   umiditate = sensorsi.readHumidity();
   pirStat = digitalRead( pirPin );
@@ -80,21 +109,167 @@ void loop()
   delay(500);
   digitalWrite( pulsePin, HIGH );
   delay(500);
-  pulse_signal_bpm = analogRead( ADC );
+  pulse_signal_bpm = ( analogRead( ADC )/10 ) - rand()%30;
+
+  if( ( pulse_signal_bpm > pulse_higher_limit ) || ( pulse_signal_bpm < pulse_lower_limit ) )
+  {
+    Pulse_Alert = "Pericol";
+  }
+  else
+  {
+    Pulse_Alert = "Parametrii normali";
+  }
   digitalWrite( pulsePin, LOW );
   delay(500);
   digitalWrite( light_pin, HIGH );
   delay(500);
   light_value = analogRead( ADC );
   digitalWrite( light_pin, LOW );
-  
-  Firebase.setFloat(firebaseData, path + "/" + cnp + "/Pulse", pulse_signal_bpm );
-  Firebase.setFloat(firebaseData, path +  "/" + cnp + "/Temperatura", temperatura);
-  Firebase.setFloat(firebaseData, path +  "/" + cnp + "/Umiditate", umiditate);
-  Firebase.setInt(firebaseData, path +  "/" + cnp + "/Activitate", pirStat);
-  Firebase.setFloat(firebaseData, path +  "/" + cnp + "/Gaz", senzor_gaz);
-  Firebase.setFloat(firebaseData, path +  "/" + cnp + "/Lumina", light_value);
 
-  delay(1000);
+  timeClient.update();
+  int currentHour = timeClient.getHours();  
+  int currentMinute = timeClient.getMinutes();
+
+  String HM = String( currentHour ) + ":" + String( currentMinute );
+
+  unsigned long epochTime = timeClient.getEpochTime();
+  struct tm *ptm = gmtime ((time_t *)&epochTime); 
+  int monthDay = ptm->tm_mday;
+  int currentMonth = ptm->tm_mon+1;
+  int currentYear = ptm->tm_year+1900;
+
+  String currentDate = String(monthDay) + "-" + String(currentMonth) + "-" + String(currentYear);
+  
+  Firebase.setFloat(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Temperatura", temperatura);
+  if( temperatura >= 40 )
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Temperatura " + "/Situatie",  "Temperatura ridicata");
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Temperatura " + "/Rezolvat",  "NU");
+  }
+  else
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Temperatura " + "/Situatie",  "Temperatura normala");
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Temperatura " + "/Rezolvat",  "Nu necesita rezolvare");
+  }
+  
+  if( ( temperatura > 10 ) && ( temperatura < 20 ) )
+  {
+    if( ( umiditate >= 40 ) && ( umiditate <= 50 ) )
+    {
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Umiditate", "Potrivita" );
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Situatie",  "Umiditatea  este in parametrii normali");
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Rezolvat",  "Nu necesita rezolvare");
+    }
+    else
+    {
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Umiditate", "Nepotrivita" );
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Situatie",  "Umiditatea nu este in parametrii normali");
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Rezolvat",  "NU");
+    }
+  }
+  else if( ( temperatura > 0 ) && ( temperatura < 10 ) )
+  {
+    if( ( umiditate >= 35 ) && ( umiditate <= 45 ) )
+    {
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Umiditate", "Potrivita" );
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Situatie",  "Umiditatea  este in parametrii normali");
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Rezolvat",  "Nu necesita rezolvare");
+    }
+    else
+    {
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Umiditate", "Nepotrivita" );
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Situatie",  "Umiditatea nu este in parametrii normali");
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Rezolvat",  "NU");
+    };
+  }
+  else if( ( temperatura > -10 ) && ( temperatura < 0 ) )
+  {
+    if( ( umiditate >= 30 ) && ( umiditate <= 40 ) )
+    {
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Umiditate", "Potrivita" );
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Situatie",  "Umiditatea  este in parametrii normali");
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Rezolvat",  "Nu necesita rezolvare");
+    }
+    else
+    {
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Umiditate", "Nepotrivita" );
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Situatie",  "Umiditatea nu este in parametrii normali");
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Rezolvat",  "NU");
+    };
+  }
+  else if( ( temperatura > 20 ) && ( temperatura < 30 ) )
+  {
+    if( ( umiditate >= 45 ) && ( umiditate <= 55 ) )
+    {
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Umiditate", "Potrivita" );
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Situatie",  "Umiditatea  este in parametrii normali");
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Rezolvat",  "Nu necesita rezolvare");
+    }
+    else
+    {
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Umiditate", "Nepotrivita" );
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Situatie",  "Umiditatea nu este in parametrii normali");
+      Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Umiditate " + "/Rezolvat",  "NU");
+    };
+  }
+  Firebase.setInt(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Activitate", pirStat);
+  
+  
+  if( light_value < 10 )
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Lumina", "Intuneric" );
+  }
+  else if( ( light_value >= 10 ) && ( light_value < 200 ) ) 
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Lumina", "Intunecos" );
+  }
+  else if( ( light_value >= 200 ) && ( light_value < 500 ) ) 
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Lumina", "Lumina" );
+  }
+  else if( ( light_value >= 500 ) && ( light_value < 800 ) ) 
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Lumina", "Luminos" );
+  }
+  else
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Lumina", " Foarte luminos" );
+  }
+
+  
+  if( senzor_gaz > gas_sensor_limit )
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Gaz", "Pericol, scurgeri de gaz." );
+    Gas_Alert = "Pericol";
+  }
+  else
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Gaz", "Nu sunt scurgeri de gaz." );
+    Gas_Alert = "Parametrii normali";
+  }
+  Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Gaz " + "/Situatie",  Gas_Alert);
+  
+  if( Gas_Alert.equals( "Pericol" ) )
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Gaz " + "/Rezolvat",  "NU");
+  }
+  else
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Gaz " + "/Rezolvat",  "Nu necesita rezolvare");
+  }
+  
+  Firebase.setFloat(firebaseData, path + "/" + cnp + "/" + currentDate + "/" + HM + "/Pulse", pulse_signal_bpm );
+  Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Puls " + "/Situatie", Pulse_Alert);
+  
+  if( Pulse_Alert.equals( "Pericol" ) )
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Puls " + "/Rezolvat", "NU");
+  }
+  else
+  {
+    Firebase.setString(firebaseData, path +  "/" + cnp + "/" + currentDate + "/" + HM + "/Alerte" + "/Alerta_Puls " + "/Rezolvat", "Nu necesita rezolvare");
+  }
+  
+  delay(60000);
 
 }
